70c23508847e9bb32604d963ec37ba1ed207618c
sv3t1k <142685645+sv3t1k@users.noreply.github.com>
2026-02-06 18:34:19 +0100

    Create main.py

---
+++ data/src/main.py
@@ -0,0 +1,14 @@
+from src.loader import DataLoader
+from src.preprocessor import TextPreprocessor
+
+def main():
+    loader = DataLoader("data/1429_1.csv")
+    df = loader.load_and_clean()
+    preprocessor = TextPreprocessor()
+    print("Starting text cleaning... (this may take a minute)")
+    df['cleaned_text'] = df['reviews.text'].apply(preprocessor.clean_text)
+    print(df[['reviews.text', 'cleaned_text', 'label']].head())
+    df.to_csv("data/processed_data.csv", index=False)
+
+if __name__ == "__main__":
+    main()
