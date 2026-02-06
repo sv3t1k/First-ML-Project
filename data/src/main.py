f5b8a1bc4c812da721257cfb5774d97045600d77
sv3t1k <142685645+sv3t1k@users.noreply.github.com>
2026-02-06 20:13:00 +0100

    Update main.py

--- data/src/main.py
+++ data/src/main.py
@@ -1,17 +1,15 @@
-from src.loader import DataLoader
-from src.preprocessor import TextPreprocessor
+from sklearn.model_selection import train_test_split
 
 def main():
-    loader = DataLoader("data/1429_1.csv")
-    df = loader.load_and_clean()
-    preprocessor = TextPreprocessor()
-    print("Starting text cleaning... (this may take a minute)")
-    df['cleaned_text'] = df['reviews.text'].apply(preprocessor.clean_text)
-    print(df[['reviews.text', 'cleaned_text', 'label']].head())
-    df.to_csv("data/processed_data.csv", index=False)
+
+    trainer = ModelTrainer(vectorizer) 
+    X = trainer.vectorize_data(df['cleaned_text'])
+    y = df['label']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    trainer.compare_models(X_train, X_test, y_train, y_test)
+    best_model = trainer.fine_tune_logistic(X_train, y_train)
+    final_preds = best_model.predict(X_test)
+    print(f"Финальная точность после тюнинга: {accuracy_score(y_test, final_preds):.4f}")
 
 if __name__ == "__main__":
     main()
-
-    print(f"Matrix is created! Size: {X.shape}") 
-
